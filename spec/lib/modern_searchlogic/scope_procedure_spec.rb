0cf350f7cfed83fba4dab6511f7989fb5ca2bd62
Zach Dennis <zach.dennis@gmail.com>
2019-08-02 10:25:35 -0400

    Rename Post.posts_for_home_page to Post.for_home_page
    so it doesn't read so funny when combining it on an association test.
    
    E.g. User.posts_for_home_page reads better than User.posts_posts_for_home_page

--- spec/lib/modern_searchlogic/scope_procedure_spec.rb
+++ spec/lib/modern_searchlogic/scope_procedure_spec.rb
@@ -9,7 +9,7 @@ describe ModernSearchlogic::ScopeProcedure do
   specify { described_class.should_not be_nil }
 
   specify do
-    Post.posts_for_home_page.to_sql.should ==
+    Post.for_home_page.to_sql.should ==
       Post.where(Post.arel_table[:published_at].not_eq(nil)).
            order(Post.arel_table[:published_at].desc).to_sql
   end
@@ -33,13 +33,13 @@ describe ModernSearchlogic::ScopeProcedure do
     end
 
     it 'can be chained on earlier scopes and scope_procedures (that return scopes)' do
-      Post.where(user_id: user_1.id).posts_for_home_page.published_grouped_by_user.should eq(
+      Post.where(user_id: user_1.id).for_home_page.published_grouped_by_user.should eq(
         user_1 => [post_1]
       )
     end
   end
 
   it 'can combine associations and scope procedures' do
-    User.posts_posts_for_home_page.to_a.should == Post.published.descend_by_published_at.map(&:user)
+    User.posts_for_home_page.to_a.should == Post.published.descend_by_published_at.map(&:user)
   end
 end
