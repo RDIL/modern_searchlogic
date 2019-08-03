8d8a0c05afea5af8d52eae522f848e2390dff40f
Zach Dennis <zach.dennis@gmail.com>
2019-08-03 11:54:18 -0400

    Re-dumping schema from Rails 5
    to get up to date version and column info.

--- spec/shared/db/schema.rb
+++ spec/shared/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2016_07_19_181005) do
+ActiveRecord::Schema.define(version: 2019_07_27_005829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
