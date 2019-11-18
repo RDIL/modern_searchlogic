c3113f20a07d9caf29701fadf86b7a6a878b96bc
Zach Dennis <zach.dennis@gmail.com>
2019-11-18 14:23:43 -0500

    Refactoring: simplify Ordering to a single include on ActiveRecord::Relation
    in a way that allows it to be work as expected when called on a model class or an ActiveRecord::Relation instance.

--- lib/modern_searchlogic/active_record_methods.rb
+++ lib/modern_searchlogic/active_record_methods.rb
@@ -8,15 +8,14 @@ require_relative 'searchable'
 module ModernSearchlogic
   module ActiveRecordMethods
     def self.install
-      ActiveRecord::Base.__send__(:include, ModernSearchlogic::ActiveRecordMethods)
-      ActiveRecord::Relation.prepend Ordering
+      ActiveRecord::Base.__send__(:include, self)
+      ActiveRecord::Relation.__send__(:include, Ordering)
     end
 
     def self.included(base)
       base.include DefaultScoping
       base.include ScopeTracking
       base.include ColumnConditions
-      base.include Ordering
       base.include ScopeProcedure
       base.include Searchable
     end
--- lib/modern_searchlogic/ordering.rb
+++ lib/modern_searchlogic/ordering.rb
@@ -1,20 +1,19 @@
 module ModernSearchlogic
   module Ordering
     def self.included(base)
-      base.extend ClassMethods
-    end
-
-    def self.prepended(base)
-      base.prepend ClassMethods
-    end
-
-    module ClassMethods
-      def order(expression)
-        if expression.to_s.match(/^(ascend|descend)_by_(.*)/) && respond_to?(expression)
-          send(expression)
-        else
-          super
+      base.module_eval do
+        define_method(:order_with_modern_searchlogic) do |*args|
+          args.reduce(self) do |scope, arg|
+            expression = arg.to_s
+            if expression.match(/^(ascend|descend)_by_(.*)/) && respond_to?(expression)
+              scope.send(expression)
+            else
+              scope.order_without_modern_searchlogic(arg)
+            end
+          end
         end
+        alias_method :order_without_modern_searchlogic, :order
+        alias_method :order, :order_with_modern_searchlogic
       end
     end
   end
