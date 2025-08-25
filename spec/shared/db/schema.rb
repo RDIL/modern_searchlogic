031206b14e8402a14ae3bbf5c505554442c48f93
Reece Dunham <me@rdil.rocks>
2025-08-25 16:50:56 -0400

    Add rails 7 test app
    We're skipping rails 6 because I do not care enough

--- spec/shared/db/schema.rb
+++ spec/shared/db/schema.rb
@@ -2,39 +2,38 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_194128) do
-
+ActiveRecord::Schema[7.2].define(version: 2019_08_12_194128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", id: :serial, force: :cascade do |t|
     t.text "body"
     t.integer "post_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "posts", id: :serial, force: :cascade do |t|
     t.integer "user_id"
     t.string "title", limit: 255
     t.text "body"
-    t.datetime "published_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "published_at", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
     t.string "username", limit: 255
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "age", default: 0, null: false
     t.string "email", limit: 255
     t.boolean "active"
@@ -46,8 +45,7 @@ ActiveRecord::Schema.define(version: 2019_08_12_194128) do
     t.string "voteable_type", limit: 255, null: false
     t.integer "vote", null: false
     t.integer "voter_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
-
 end
