RDIL/modern_searchlogic

commit 28268a7ce6de89ee5a226e8271c4a116340e2e4b

Fix column discovery running too early
In a test environment, or when creating/seeding the database, there are no tables defined in ActiveRecord. This causes an edge case where, if *any* `method_missing` is called (and it gets to MSL), an error will be thrown. This is because accessing `columns_hash` or `column_names` will cause a query to be made that will fail (as it expects the table to already be present in the database).

For getting caught up on the memes, see the following:
- https://github.com/Genius/Rap-Genius/pull/15200
- https://github.com/Genius/heroku-buildpack-ruby/pull/14

Feel free to backport this to Rails 2 - seems pointless since it's On The Way Out (TM) (as it has been for the last decade, but who's counting?!)

lib/modern_searchlogic/column_conditions.rb
@@ -6,6 +6,7 @@ module ModernSearchlogic
       end
 
       def valid_searchlogic_scope?(method)
+        return false unless connection.tables.size > 0
         return false if method =~ /^define_method_/
 
         searchlogic_scope_dynamically_defined?(method) ||
@@ -191,7 +192,7 @@ module ModernSearchlogic
       end
 
       def method_missing(method, *args, &block)
-        return super unless dynamically_define_searchlogic_method(method)
+        return super unless connection.tables.size > 0 && dynamically_define_searchlogic_method(method)
 
         __send__(method, *args, &block)
       end

commit deb6fdafe85bc92ed83d40245d44faf617ab0433

Perf: cache positive `connection.tables` result
This is a bug introduced in 28268a7ce6de89ee5a226e8271c4a116340e2e4b, which took down genius.com on May 25, 2026 for a couple of minutes before we rolled it back.

Guards dynamic-finder dispatch until the schema is available. Uses `connection.tables` instead of the schema cache, which could cache a stale "no such table" result on Rails 4/5). We memoize only the positive result, so once the schema is present we stop hitting the DB on every `method_missing` / `respond_to?` instead of spamming `SHOW TABLES`.

lib/modern_searchlogic/column_conditions.rb
@@ -6,7 +6,7 @@ module ModernSearchlogic
       end
 
       def valid_searchlogic_scope?(method)
-        return false if connection.tables.empty? || method =~ /^define_method_/ || abstract_class?
+        return false if !searchlogic_schema_ready? || method =~ /^define_method_/ || abstract_class?
 
         searchlogic_scope_dynamically_defined?(method) ||
           !!searchlogic_column_condition_method_block(method.to_s) ||
@@ -32,6 +32,10 @@ module ModernSearchlogic
       end
 
       private
+
+      def searchlogic_schema_ready?
+        @searchlogic_schema_ready ||= !connection.tables.empty?
+      end
 
       def searchlogic_scope_dynamically_defined?(method)
         _dynamically_defined_searchlogic_scopes.key?(method)
@@ -191,7 +195,7 @@ module ModernSearchlogic
       end
 
       def method_missing(method, *args, &block)
-        return super if connection.tables.empty? || abstract_class?
+        return super if !searchlogic_schema_ready? || abstract_class?
         return super unless dynamically_define_searchlogic_method(method)
 
         __send__(method, *args, &block)