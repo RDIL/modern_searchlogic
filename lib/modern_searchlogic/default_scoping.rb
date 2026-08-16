afdac5fab6030c1ca12408898b9059e022219095
Reece Dunham <me@rdil.rocks>
2026-08-16 16:28:19 -0400

    Fix deprecation warning in `searchlogic_default_scope`
    On Rails 4, `scoped` still exists but is deprecated, so just avoid it explicitly.

--- lib/modern_searchlogic/default_scoping.rb
+++ lib/modern_searchlogic/default_scoping.rb
@@ -1,13 +1,15 @@
 module ModernSearchlogic
   module DefaultScoping
+    extend ActiveSupport::Concern
+
     module ClassMethods
       def searchlogic_default_scope
-        respond_to?(:scoped) ? scoped : all
+        if Rails::VERSION::MAJOR < 4
+          scoped
+        else
+          all
+        end
       end
-    end
-
-    def self.included(base)
-      base.extend ClassMethods
     end
   end
 end
